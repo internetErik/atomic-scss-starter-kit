846347036603aa6468ac805bab780087f0dc2952
Erik Christianson <internet.erik@gmail.com>
2015-06-23 15:57:05 -0500

    major refactor. now this is an express app.  I have also moved back to emmet class names

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,55 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	uncss = require('gulp-uncss'),
-	autoprefixer = require('gulp-autoprefixer');
+    sass = require('gulp-sass'),
+    uncss = require('gulp-uncss'),
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer');
 
+/*
+ * Create variables for our project paths so we can change in one place
+ */
+var paths = {
+    'sass': './public/stylesheets/**/*.scss',
+    'css': './public/stylesheets/',
+    'blitzCss': './public/stylesheets/blitz.css',
+    'allCss': './public/stylesheets/*.css',
+    'hbs': ['./views/**/*.hbs', './views/*.hbs'],
+    'builtHtml': './assembled_hbs/',
+    'html': './assembled_hbs/site.html'
+};
+
+gulp.task('assemble', function () {
+  return gulp.src(paths.hbs)
+      .pipe(concat('site.html'))
+      .pipe(gulp.dest(paths.builtHtml));
+});
+
+gulp.task('uncss', function(){
+  return gulp.src(paths.blitzCss)
+        .pipe(uncss({
+            html: [paths.html],
+            options: {
+              ignoreSslErrors: 'true'
+            }
+        }))
+        .pipe(gulp.dest(paths.css));
+});
 
 gulp.task('sass', function(){
-	return gulp.src('./styles/**/*.scss')
-		.pipe(sass({errLogToConsole: true}))
-        // use this when building for production
-        // .pipe(uncss({
-        //     html: ['index.html']
-        // }))
-		.pipe(autoprefixer('last 2 versions'))
-		.pipe(gulp.dest('./styles'));
+  return gulp.src(paths.sass)
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefixer({browsers: ['last 2 versions', 'IE 9']}))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('buildCss', ['assemble','sass','uncss'], function(){
+  return gulp.src(paths.allCss)
+    .pipe(concat('site.css'))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('watch', function() {
+
+    gulp.start('buildCss');
+    gulp.watch(paths.sass, ['buildCss']);
+
 });
